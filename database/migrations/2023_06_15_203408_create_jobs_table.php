e0acd7a4ef84ba4b16b90684c2cb5982cc27b5ab
AmaanBaig1 <101422421+AmaanBaig1@users.noreply.github.com>
2023-06-17 19:40:54 +0500

    final change

--- database/migrations/2023_06_15_203408_create_jobs_table.php
+++ database/migrations/2023_06_15_203408_create_jobs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->string('type');
+            $table->string('type_id');
             $table->string('title');
             $table->string('rate');
             $table->longText('description');
